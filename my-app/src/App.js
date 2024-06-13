8e48b6f92c6bd3d0e570a15a89c485907f7f60f4
charily-krish <muralikrishna.lucid@gmail.com>
2024-06-13 15:16:56 +0530

    Latest Change in New Feature

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React App for New features</p>
+        <p>React App for New features for Branch on June 13 2024</p>
         <a
           className="App-link"
           href="https://reactjs.org"
